f3686504d508ff049aa806c592d742fd98094742
Netter <msn589@wireless-10-146-10-82.public.utexas.edu>
2018-12-07 12:48:37 -0600

    Updated custbarplot() to accomodate calculation of percentages within group

--- R/custbarplot.R
+++ R/custbarplot.R
@@ -2,44 +2,49 @@
 #'
 #' @param data Name of dataset (must be in wide format, i.e., one row per participant)
 #' @param xvar Name of variable of interest measured on Likert scale
-#' @param groupvar Name of variable of type factor by which bars should be split
+#' @param grpvar Name of variable of type factor by which bars should be split
+#' @param withingrp Logical value indicating whether or not percentages should be calculated within groups
 #' @param scalepoints Numeric value indicating number of scalepoints in Likert scale (at present, can take on value of 5 or 7)
 #' @param decimals Logical value indicating whether or not xvar is on a non-integer scale
 #' @param scale Character value indicating scale labels (at present, can take on values of "Agree", "Quant", or "True")
 #' @param ... Other arguments to pass on to ggplot
 #' @return A barplot showing the counts and percentages of all values of xvar
-#' @importFrom lazyeval "lazy"
-#' @importFrom scales "percent"
 #' @importFrom magrittr "%>%"
 #' @importFrom ggplot2 "aes"
+#' @importFrom ggplot2 "element_rect"
 #' @importFrom ggplot2 "geom_col"
 #' @importFrom ggplot2 "geom_text"
+#' @importFrom ggplot2 "labs"
 #' @importFrom ggplot2 "position_dodge2"
 #' @importFrom ggplot2 "position_dodge"
-#' @importFrom ggplot2 "labs"
-#' @importFrom ggplot2 "scale_y_continuous"
-#' @importFrom ggplot2 "scale_x_continuous"
 #' @importFrom ggplot2 "scale_fill_discrete"
-#' @importFrom ggplot2 "theme_bw"
+#' @importFrom ggplot2 "scale_x_continuous"
+#' @importFrom ggplot2 "scale_y_continuous"
 #' @importFrom ggplot2 "theme"
+#' @importFrom ggplot2 "theme_bw"
 #' @examples
 #' mtcars$am <- as.factor(mtcars$am)
-#' custbarplot(data = mtcars, xvar = gear, groupvar = am, scalepoints = 5, decimals = FALSE)
+#' custbarplot(mtcars, xvar = gear, grpvar = am, withingrp = TRUE, scalepoints = 5, decimals = FALSE)
 #' @export
 
-custbarplot <- function(data, xvar, groupvar, scalepoints = 7, decimals = TRUE, scale = "Agree", ...) {
+custbarplot <- function(data, xvar, grpvar, withingrp = FALSE, scalepoints = 7, decimals = TRUE, scale = "Agree", ...) {
 
   n <- NULL
+  xvar1 <- rlang::enquo(xvar)
+  grpvarxvar <- rlang::enquos(grpvar, xvar)
 
-  if (missing(groupvar)) {
+  if (missing(grpvar)) {
     count_dataset <- data %>%
-      dplyr::count(.dots = lazyeval::lazy(xvar)) %>%
+      dplyr::group_by(!! xvar1) %>%
+      dplyr::summarize(n = n()) %>%
+      dplyr::mutate(per = scales::percent(n / sum(n))) %>%
       tidyr::replace_na(list(n = 0)) # replace any NA values in n column with 0
+    #return(count_dataset)
 
     out <- ggplot2::ggplot(count_dataset,
                            aes(x = dplyr::pull(count_dataset, 1),
                                y = dplyr::pull(count_dataset, 2))) +
-      geom_col(position = position_dodge2(preserve = "single")) +
+      geom_col(position = position_dodge2(preserve = "total")) +
       labs(x = lazyeval::lazy(xvar),
            y = "count") +
       geom_text(aes(label = scales::percent(n / sum(n)),
@@ -65,41 +70,93 @@ custbarplot <- function(data, xvar, groupvar, scalepoints = 7, decimals = TRUE, 
       theme_bw() +
       theme()
 
-  } else {
+    #return(out)
+    #return(list(count_dataset, out))
+  } else if (!missing(grpvar) & withingrp == TRUE){
     count_dataset <- data %>%
-      dplyr::group_by(.dots = lazyeval::lazy(groupvar)) %>%
-      dplyr::count(.dots = lazyeval::lazy(xvar)) %>%
-      tidyr::replace_na(list(n = 0)) # replace any NA values in n column with 0
+      dplyr::group_by(!!! grpvarxvar) %>%
+      dplyr::summarize(n = n()) %>%
+      dplyr::ungroup() %>%
+      tidyr::complete(!!! grpvarxvar) %>%
+      dplyr::arrange(!! xvar1) %>%
+      tidyr::replace_na(list(n = 0)) %>% # replace any NA values in n column with 0
+      dplyr::group_by(!! xvar1) %>%
+      dplyr::mutate(per = scales::percent(n / sum(n)))
+    #return(count_dataset)
 
     out <- ggplot2::ggplot(count_dataset, aes(x = dplyr::pull(count_dataset, 2),
-                                              y = dplyr::pull(count_dataset, 3),
-                                              fill = dplyr::pull(count_dataset, 1))) +
-      geom_col(position = position_dodge2(preserve = "single")) +
-      labs(x = lazyeval::lazy(xvar),
-           y = "count") +
-      scale_fill_discrete(name = lazyeval::lazy(groupvar)) +
-      geom_text(aes(label = scales::percent(n / sum(n)),
-                    y = n),
-            vjust = -.5,
-            size = 3,
-            position = position_dodge(width = .8)) +
-      geom_text(aes(label = n,
-                    y = n),
-            vjust = -2,
-            hjust = -.15,
-            size = 3,
-            position = position_dodge(width = .8)) +
-      geom_text(aes(label = "n = ",
-                    y = n),
-            vjust = -2,
-            hjust = .85,
-            size = 3,
-            position = position_dodge(width = .8)) +
-      scale_y_continuous(limits = c(0, max(dplyr::pull(count_dataset, 3)) * 1.1),
-                         breaks = seq(0, max(dplyr::pull(count_dataset, 3)) * 1.1,
-                                      by = 5)) +
-      theme_bw() +
-      theme()
+                                                y = dplyr::pull(count_dataset, 3),
+                                                fill = dplyr::pull(count_dataset, 1))) +
+        geom_col(position = position_dodge2(preserve = "single")) +
+        labs(x = lazyeval::lazy(xvar),
+             y = "count") +
+        scale_fill_discrete(name = lazyeval::lazy(grpvar)) +
+        geom_text(aes(label = dplyr::pull(count_dataset, 4),
+                      y = n),
+                  vjust = -.5,
+                  size = 3,
+                  position = position_dodge(width = .8)) +
+        geom_text(aes(label = n,
+                      y = n),
+              vjust = -2,
+              hjust = -.15,
+              size = 3,
+              position = position_dodge(width = .8)) +
+        geom_text(aes(label = "n = ",
+                      y = n),
+              vjust = -2,
+              hjust = .85,
+              size = 3,
+              position = position_dodge(width = .8)) +
+        scale_y_continuous(limits = c(0, max(dplyr::pull(count_dataset, 3)) * 1.1),
+                           breaks = seq(0, max(dplyr::pull(count_dataset, 3)) * 1.1,
+                                        by = 5)) +
+        theme_bw() +
+        theme(legend.position = c(.9,.9), legend.box.background = element_rect(colour = "black"))
+    #return(out)
+    #return(list(count_dataset, out))
+  } else {
+    count_dataset <- data %>%
+      dplyr::group_by(!!! grpvarxvar) %>%
+      dplyr::summarize(n = n()) %>%
+      dplyr::ungroup() %>%
+      tidyr::complete(!!! grpvarxvar) %>%
+      dplyr::arrange(!! xvar1) %>%
+      tidyr::replace_na(list(n = 0)) %>% # replace any NA values in n column with 0
+      dplyr::mutate(per = scales::percent(n / sum(n)))
+    #return(count_dataset)
+
+    out <- ggplot2::ggplot(count_dataset, aes(x = dplyr::pull(count_dataset, 2),
+                                                y = dplyr::pull(count_dataset, 3),
+                                                fill = dplyr::pull(count_dataset, 1))) +
+        geom_col(position = position_dodge2(preserve = "single")) +
+        labs(x = lazyeval::lazy(xvar),
+             y = "count") +
+        scale_fill_discrete(name = lazyeval::lazy(grpvar)) +
+        geom_text(aes(label = dplyr::pull(count_dataset, 4),
+                      y = n),
+                  vjust = -.5,
+                  size = 3,
+                  position = position_dodge(width = .8)) +
+        geom_text(aes(label = n,
+                      y = n),
+              vjust = -2,
+              hjust = -.15,
+              size = 3,
+              position = position_dodge(width = .8)) +
+        geom_text(aes(label = "n = ",
+                      y = n),
+              vjust = -2,
+              hjust = .85,
+              size = 3,
+              position = position_dodge(width = .8)) +
+        scale_y_continuous(limits = c(0, max(dplyr::pull(count_dataset, 3)) * 1.1),
+                           breaks = seq(0, max(dplyr::pull(count_dataset, 3)) * 1.1,
+                                        by = 5)) +
+        theme_bw() +
+        theme(legend.position = c(.9,.9), legend.box.background = element_rect(colour = "black"))
+    #return(out)
+    #return(list(count_dataset, out))
     }
 
   if (scalepoints != 5 & scalepoints != 7){
